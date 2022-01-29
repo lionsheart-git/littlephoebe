a27936d8a3f5da4ecaac1b4e42bd49a8ea8bb822
Patrick Glöckner <p.gloeckner@tu-braunschweig.de>
2022-01-29 19:43:01 +0100

    RO-12 Added walking of directories

--- code/arduino/pyarduino_helper/Library.py
+++ code/arduino/pyarduino_helper/Library.py
@@ -2,4 +2,10 @@ class Library:
 
     @staticmethod
     def index(path: str):
-        print(path)
+        import os
+        for root, dirs, files in os.walk(path):
+            # print('root: ', root)
+            path = root.split(os.sep)
+            print((len(path) - 1) * '  ', os.path.basename(root), '/')
+            for file in files:
+                print(len(path) * '  ', file)

6619ad8a4e898243f9e3785ca52e89e2a689657f
Patrick Glöckner <p.gloeckner@tu-braunschweig.de>
2022-01-30 20:22:19 +0100

    RO-12 Added some formatting

--- code/arduino/pyarduino_helper/Cmake.py
+++ code/arduino/pyarduino_helper/Cmake.py
@@ -4,30 +4,30 @@ import os
 class CMakeLists:
 
     def __init__(self, lib_name: str, path: str, mode: str):
-        self.__lib_name = lib_name
+        self.__lib_name = f'{lib_name}_lib'
         self.__file = open(f'{path}{os.sep}CMakeLists.txt', mode)
 
     def generate_arduino_library(self, headers: str, sources: str, libs: str = None, board: str = None):
         string = f'generate_arduino_library({self.__lib_name}\n'
         if sources:
-            string += f'SRCS {sources}\n'
+            string += f'\tSRCS {sources}\n'
 
         if headers:
-            string += f'HDRS {headers}\n)'
+            string += f'\tHDRS {headers}\n)'
 
         if libs:
-            string += f'LIBS {libs}'
+            string += f'\tLIBS {libs}'
 
         if board:
-            string += f'BOARD {board}'
+            string += f'\tBOARD {board}'
 
-        self.__file.write(string)
+        self.__file.write(f'{string}\n')
 
     def target_include_directory(self):
-        self.__file.write(f'target_include_directories({self.__lib_name} PUBLIC ${{CMAKE_CURRENT_SOURCE_DIR}})')
+        self.__file.write(f'target_include_directories({self.__lib_name} PUBLIC ${{CMAKE_CURRENT_SOURCE_DIR}})\n')
 
     def add_subdirectory(self, subdirectory: str):
-        self.__file.write(f'add_subdirectory({subdirectory})')
+        self.__file.write(f'add_subdirectory({subdirectory})\n')
 
     def add_subdirectories(self, subdirectories: list):
         for subdir in subdirectories:

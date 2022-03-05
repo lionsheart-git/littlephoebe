ff8b3e866bad5d4746055e4f0e113c064cb04bb3
Patrick Gloeckner <p.gloeckner@tu-braunschweig.de>
2022-03-05 13:06:14 +0000

    RO-19 Crated basic logger for logging to serial

---
+++ code/esp32/FlightComputer/src/Logger.cpp
@@ -0,0 +1,69 @@
+#include <cstdarg>
+#include <cstring>
+#include <cstdio>
+
+// #include <esp32-hal-log.h>
+#include <esp_attr.h>
+
+#include "Logger.hpp"
+#include "esp32/tools/sdk/include/newlib/malloc.h"
+
+uart_t * uart0_ = uartBegin(0, 115200, SERIAL_8N1, 3, 1, 0, false);
+
+/**
+ * Extracts only file from path given by __FILE__.
+ *
+ * @param path Path of the file.
+ * @return The file name.
+ */
+const char * IRAM_ATTR Logger::PathToFileName(const char * path)
+{
+    size_t i = 0;
+    size_t pos = 0;
+    char * p = (char *)path;
+    while(*p){
+        i++;
+        if(*p == '/' || *p == '\\'){
+            pos = i;
+        }
+        p++;
+    }
+    return path+pos;
+}
+
+/**
+ * @brief Adaptation of log_printf from esp32-hal-log.h.
+ *
+ * @param format
+ * @param ...
+ * @return
+ */
+int Logger::log(const char *format, ...)
+{
+    static char loc_buf[64];
+    char * temp = loc_buf;
+    int len;
+    va_list arg;
+    va_list copy;
+    va_start(arg, format);
+    va_copy(copy, arg);
+    len = vsnprintf(NULL, 0, format, arg);
+    va_end(copy);
+    if(len >= sizeof(loc_buf)){
+        temp = (char*)malloc(len+1);
+        if(temp == NULL) {
+            return 0;
+        }
+    }
+    vsnprintf(temp, len+1, format, arg);
+
+    // Write to Serial (UART) and / or SDCard
+    uartWriteBuf(uart0_, (uint8_t const *) temp, len);
+
+    va_end(arg);
+    if(len >= sizeof(loc_buf)){
+        free(temp);
+    }
+    return len;
+}
+

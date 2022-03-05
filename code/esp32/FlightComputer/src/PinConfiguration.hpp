e8d2425e510fb61aa6eea3f3be263c3a41483944
Patrick Gloeckner <p.gloeckner@tu-braunschweig.de>
2022-03-05 13:13:07 +0000

    Created file for pin configuration

---
+++ code/esp32/FlightComputer/src/PinConfiguration.hpp
@@ -0,0 +1,17 @@
+
+#ifndef FLIGHTCOMPUTER_PINCONFIGURATION_HPP
+#define FLIGHTCOMPUTER_PINCONFIGURATION_HPP
+
+#define LoRa_SCK 5 /**<@brief Used by LoRa module */
+#define LoRa_CS 18 /**<@brief Used by LoRa module */
+#define LoRa_MISO 19 /**<@brief Used by LoRa module */
+#define LoRa_MOSI 27 /**<@brief Used by LoRa module */
+#define LoRa_DIO0 26 /**<@brief Used by LoRa module */
+#define LoRa_RST 14 /**<@brief Used by LoRa module */
+
+#define SD_SCK 12
+#define SD_CS 13
+#define SD_MISO 32
+#define SD_MOSI 33
+
+#endif //FLIGHTCOMPUTER_PINCONFIGURATION_HPP

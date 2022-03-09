ba59cfcfb003c8c101a6e77d435e341413ef59d9
Patrick Gloeckner <p.gloeckner@tu-braunschweig.de>
2022-03-09 17:21:33 +0000

    RO-19 Adapted system logger for needs of data logger

--- code/esp32/FlightComputer/src/Logging/DataLogger.hpp
+++ code/esp32/FlightComputer/src/Logging/DataLogger.hpp
@@ -1,19 +1,43 @@
-
 #ifndef FLIGHTCOMPUTER_DATALOGGER_HPP
 #define FLIGHTCOMPUTER_DATALOGGER_HPP
 
 #include "Logger.hpp"
 
-namespace Logging
-{
+#ifndef D_LOG_PATH
+#define D_LOG_PATH "/datalog.log"
+#endif
 
-    class DataLogger
-            : Logger
-    {
+#ifndef SHORT_D_LOG_FORMAT//(letter, format)
+#define SHORT_D_LOG_FORMAT(format) format
+#endif
 
-    };
+#ifndef SHORT_D_LOGN_FORMAT//(letter, format)
+#define SHORT_D_LOGN_FORMAT(format) format "\r\n"
+#endif
 
-}
+//@todo Implemented using of rtc time
+#ifndef D_LOG_FORMAT//(letter, format)
+#define D_LOG_FORMAT(format) "[%ld] " format, millis()
+#endif
 
+#ifndef D_LOGN_FORMAT//(letter, format)
+#define D_LOGN_FORMAT(format) "[%ld] " #format "\r\n", millis()
+#endif
+
+#ifndef dlog
+#define dlog(format, ...) Logging::Logger::Log(D_LOG_PATH, D_LOG_FORMAT(format), ##__VA_ARGS__)
+#endif
+
+#ifndef dlogn
+#define dlogn(format, ...) Logging::Logger::Log(D_LOG_PATH, D_LOGN_FORMAT(format), ##__VA_ARGS__)
+#endif
+
+#ifndef dlogs
+#define dlogs(format, ...) Logging::Logger::Log(D_LOG_PATH, SHORT_D_LOG_FORMAT(format), ##__VA_ARGS__)
+#endif
+
+#ifndef dlogsn
+#define dlogsn(format, ...) Logging::Logger::Log(D_LOG_PATH, SHORT_D_LOGN_FORMAT(format), ##__VA_ARGS__)
+#endif
 
 #endif //FLIGHTCOMPUTER_DATALOGGER_HPP

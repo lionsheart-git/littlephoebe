3ce94c4549f65f3f1a45d02de95ced76a591e79e
Patrick Gloeckner <p.gloeckner@tu-braunschweig.de>
2022-03-10 16:06:29 +0000

    RO-21 Documented constants

--- code/esp32/FlightComputer/src/Constants.hpp
+++ code/esp32/FlightComputer/src/Constants.hpp
@@ -1,6 +1,18 @@
+/**
+ * @file Constants.hpp
+ * @brief Collection of constants used throughout the program.
+ */
+
 #ifndef FLIGHTCOMPUTER_CONSTANTS_HPP
 #define FLIGHTCOMPUTER_CONSTANTS_HPP
 
+/**
+ * @def EPSILON
+ * @brief General threshold to add or subtract.
+ *
+ * Due to numerical instability values sometimes are not as exact as expected after an operation.
+ * Here EPSILON can be used to account for that.
+ */
 #ifndef EPSILON
 #define EPSILON 1E-4
 #endif

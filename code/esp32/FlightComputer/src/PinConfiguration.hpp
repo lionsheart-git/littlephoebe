56284ff2a36ddc3a143ee93c001c48c0a54eaa54
Patrick Gloeckner <p.gloeckner@tu-braunschweig.de>
2022-03-10 16:12:11 +0000

    RO-21 Documented pin configuration.

--- code/esp32/FlightComputer/src/PinConfiguration.hpp
+++ code/esp32/FlightComputer/src/PinConfiguration.hpp
@@ -1,17 +1,22 @@
-
+/**
+ * @file PinConfiguration.hpp
+ * @brief Default pin configuration used throughout the program.
+ *
+ * Could later be done through arguments to give full versatility.
+ */
 #ifndef FLIGHTCOMPUTER_PINCONFIGURATION_HPP
 #define FLIGHTCOMPUTER_PINCONFIGURATION_HPP
 
-#define LoRa_SCK 5 /**<@brief Used by LoRa module */
-#define LoRa_CS 18 /**<@brief Used by LoRa module */
-#define LoRa_MISO 19 /**<@brief Used by LoRa module */
-#define LoRa_MOSI 27 /**<@brief Used by LoRa module */
-#define LoRa_DIO0 26 /**<@brief Used by LoRa module */
-#define LoRa_RST 14 /**<@brief Used by LoRa module */
+#define LoRa_SCK 5 /**< @brief Used by LoRa module */
+#define LoRa_CS 18 /**< @brief Used by LoRa module */
+#define LoRa_MISO 19 /**< @brief Used by LoRa module */
+#define LoRa_MOSI 27 /**< @brief Used by LoRa module */
+#define LoRa_DIO0 26 /**< @brief Used by LoRa module */
+#define LoRa_RST 14 /**< @brief Used by LoRa module */
 
-#define SD_SCK 12
-#define SD_CS 13
-#define SD_MISO 32
-#define SD_MOSI 33
+#define SD_SCK 12 /**< @brief SD clock pin */
+#define SD_CS 13 /**< @brief SD chip select pin */
+#define SD_MISO 32 /**< @brief SD miso pin */
+#define SD_MOSI 33 /**< @brief SD mosi pin */
 
 #endif //FLIGHTCOMPUTER_PINCONFIGURATION_HPP
